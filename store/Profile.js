d13f4ab5c99ca49c5c33cfac20b245d77d1d4077
Ezra Rabinsky <ezrarabinsky@Ezras-MacBook-Pro.local>
2019-10-19 12:50:24 -0500

    create dummy profile in 'store' directory

---
+++ store/Profile.js
@@ -0,0 +1,11 @@
+export default {
+  heading: "Client Name",
+  main: `
+        <img src="https://picsum.photos/id/555/200/200/>
+        <span> Today's Date</span>
+        <span> Today's Menu</span>
+        <span> Spots Available: 20</span>
+        <a href="#">Reserve Now!</a>
+        <a href="#">Calendar</a>
+        <a href="#" id="help">Help</a>`
+}
