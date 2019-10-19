54b386d47306d4a21dd08542b95d42e0c8ee3026
Ezra Rabinsky <ezrarabinsky@Ezras-MacBook-Pro.local>
2019-10-19 14:06:25 -0500

    Create dynamic navigation in ./compononents.Nav.js

--- components/Nav.js
+++ components/Nav.js
@@ -1,1 +1,11 @@
+export default () => {
+  `<nav>
+    <div class="fas fa-bars"
+    <ul class= "navLinks">
+      <li><a href="../store/Profile"></a>My Profile</li>
+      <li><a href="../store/Reservations">My Reservations</a></li>
+      <li><a href="../store/Contact">Contact Us</a></li>
+      <li><a href="../store/Help">Help</a></li>
 
+  </nav>`
+}
