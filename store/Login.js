2a3d3c4479cddd89f5608332d60434b27d229fd5
Ezra Rabinsky <ezrarabinsky@Ezras-MacBook-Pro.local>
2019-10-19 14:21:50 -0500

    refactor login from index.html to ./store/Login.js

---
+++ store/Login.js
@@ -0,0 +1,22 @@
+export default {
+heading: "Welcome",
+main:
+  `<div class="container">
+<form action="#">
+  <div class="input">
+
+    <div class="input-line">
+    <label for="name">Name</label>
+    <input type="text" name="name" id="name" />
+  </div>
+    <div class="input-line">
+    <label for="password">Password</label>
+    <input type="text" name="password" id="password" />
+    </div>
+  <input type="Submit" value="Submit" />
+</div>
+</form>
+</div>
+<ul class="links">
+<li><a href="./docs/Profile/create_profile.html">New User</a></li`
+}
