54f4471506cdaada4ae808b920cb07217adc3415
Ezra Rabinsky <ezrarabinsky@Ezras-MacBook-Pro.local>
2019-10-19 13:57:10 -0500

    edit Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,10 +1,7 @@
 
 export default () =>  `<footer>
-<ul>
-  <li><a href="./index.html">Profile</a></li>
-  <li><a href="./Reservations/">Reservations</a></li>
-  <li><a href="./Help/">Help</a></li>
-  <li><a href="./Contact/">Contact</a></li>
-</ul>
+<p>
+  <a href="./Help/">Help</a></<a>
+</p>
 &copy; Ezra Rabinsky 2019. All rights reserved.
 </footer>`;
