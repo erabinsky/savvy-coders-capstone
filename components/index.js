76e19990d3d31aef950d237bc58712cb944649f8
Ezra Rabinsky <ezrarabinsky@Ezras-MacBook-Pro.local>
2019-10-19 14:12:59 -0500

    Export all js files in ./components via ./components/index.js

---
+++ components/index.js
@@ -0,0 +1,7 @@
+//gathers export information from each file in the ./components directory and
+//exports them to be used by index.js in the root directory
+
+export {default as Header} from './Header';
+export {default as Nav} from './Nav';
+export {default as Main} from './Main';
+export {default as Footer} from './Footer';
