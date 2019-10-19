72b33773099a6634737115803426377a931b47f9
Ezra Rabinsky <ezrarabinsky@Ezras-MacBook-Pro.local>
2019-10-19 14:37:08 -0500

    temporarily remove firebase script from index.html and begin refactoring components and store imports in ~/index.js

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 import { Header, Nav, Main, Footer } from "./components";
 import * as state from "./store";
+
+
+function render(st = state.Login){
+
+  (document.querySelector("#root").innerHTML = `
+  ${Header(st)}
+  ${Nav()}
+  ${Main(st)}
+  ${Footer()}
+  `)};
+
+  render();
--- store/index.js
+++ store/index.js
@@ -1,4 +1,5 @@
+export {default as Login} from './Login';
 export {default as Profile} from './Profile';
 export {default as Reservations} from './Reservations';
 export {default as Help} from './Help';
-export {default as Contact} from './Profile';
+export {default as Contact} from './Contact';
