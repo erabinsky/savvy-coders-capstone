3af2acc7fabbaed811508d44d316e37b19256957
Ezra Rabinsky <ezrarabinsky@Ezras-MacBook-Pro.local>
2019-10-19 13:12:30 -0500

    create dummy contact view in 'store' directory

---
+++ store/Contact.js
@@ -0,0 +1,12 @@
+export default {
+  heading: "Contact",
+  main: `
+
+  <span> email: <a href="mailto:email email@email.com">email email@email.com</a></span>
+  <span> phone: <a href="tel:(123)4567890"> (123) 456-7890 </a> </span>
+  <span> mailing address:
+                          <br> Place Name </br>
+                          <br> 123 Street Road </br>
+                          <br> City, ST 12345 </br>
+  `
+}
