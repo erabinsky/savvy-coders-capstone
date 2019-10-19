df33fd2625b336acc8f7d13eb09d01b317eefac5
Ezra Rabinsky <ezrarabinsky@Ezras-MacBook-Pro.local>
2019-10-19 13:18:27 -0500

    Add store/index.js and store/navLinks.s and set up exports for all 'store' files via store/index.js

---
+++ store/index.js
@@ -0,0 +1,4 @@
+export {default as Profile} from './Profile';
+export {default as Reservations} from './Reservations';
+export {default as Help} from './Help';
+export {default as Contact} from './Profile';
