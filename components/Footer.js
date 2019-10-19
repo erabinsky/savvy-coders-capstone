3a99c2c69c431319398de10649023ab3118003bd
Ezra Rabinsky <ezrarabinsky@Ezras-MacBook-Pro.local>
2019-10-19 13:55:31 -0500

    Create dynamic footer in ./components/Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,10 @@
+
+export default () =>  `<footer>
+<ul>
+  <li><a href="./index.html">Profile</a></li>
+  <li><a href="./Reservations/">Reservations</a></li>
+  <li><a href="./Help/">Help</a></li>
+  <li><a href="./Contact/">Contact</a></li>
+</ul>
+&copy; Ezra Rabinsky 2019. All rights reserved.
+</footer>`;
