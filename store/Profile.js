626bcb24ccec1339641e5770da282620b7609dee
Ezra Rabinsky <ezrarabinsky@Ezras-MacBook-Pro.local>
2019-10-19 13:00:28 -0500

    Create dummy client reservations view in 'store' directory

--- store/Profile.js
+++ store/Profile.js
@@ -1,7 +1,7 @@
 export default {
   heading: "Client Name",
   main: `
-        <img src="https://picsum.photos/id/555/200/200/>
+        <img src="https://picsum.photos/id/555/200/200"/>
         <span> Today's Date</span>
         <span> Today's Menu</span>
         <span> Spots Available: 20</span>
---
+++ store/Reservations.js
@@ -0,0 +1,17 @@
+export default{
+  heading:"My Calendar",
+  main: `
+    <img src="https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwip_Mnh8ajlAhXLs54KHSjGD0kQjRx6BAgBEAQ&url=https%3A%2F%2Ftemplates.office.com%2Fen-us%2Fclassic-wall-calendar-mon-sun-two-ring-design-tm16401331&psig=AOvVaw0PZEBbi3mCWTxjIWhsa4y7&ust=1571593930683524"/>
+    <span> Today's Date: October 19, 2019 </span>
+    <span>  October 21, 2019 | Grilled Chicken Breast </span>
+    <span> Spots available: 15 </span>
+    <a href="#">Reserve Now!</a>
+
+    <a href="#">Preferences</a>
+    <a href="#">Restrictions</a>
+    <a href="#">Schedule</a>
+    <a href="#">Menu Search</a>
+    <a href="#">Help</a>
+
+  `
+}
