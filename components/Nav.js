727204a2d3ec81a1b3d53bfbe5cb094dd8246995
Ezra Rabinsky <ezrarabinsky@Ezras-MacBook-Pro.local>
2019-10-22 11:02:38 -0500

    Refactor links in Nav.js to state

--- components/Nav.js
+++ components/Nav.js
@@ -2,9 +2,9 @@ export default () =>
   `<nav>
     <div class="fas fa-bars"
     <ul class= "navLinks">
-      <li><a href="../store/Profile"></a>My Profile</li>
-      <li><a href="../store/Reservations">My Reservations</a></li>
-      <li><a href="../store/Contact">Contact Us</a></li>
-      <li><a href="../store/Help">Help</a></li>
+      <li><a href="Profile"></a>My Profile</li>
+      <li><a href="Reservations">My Reservations</a></li>
+      <li><a href="Contact">Contact Us</a></li>
+      <li><a href="Help">Help</a></li>
 
   </nav>`;
