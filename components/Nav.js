7f59195db2b250b2b03af703ead60cafb06ac200
Ezra Rabinsky <ezrarabinsky@Ezras-MacBook-Pro.local>
2019-10-22 11:10:31 -0500

    Add data-navigo attributes to Nav.js anchors

--- components/Nav.js
+++ components/Nav.js
@@ -3,10 +3,10 @@ export default () =>`
     <span class="fas fa-bars">
     </span>
     <ul class= "navLinks">
-      <li><a href="Profile">My Profile</a></li>
-      <li><a href="Reservations">My Reservations</a></li>
-      <li><a href="Contact">Contact Us</a></li>
-      <li><a href="Help">Help</a></li>
+      <li><a data-navigo href="Profile">My Profile</a></li>
+      <li><a data-navigo href="Reservations">My Reservations</a></li>
+      <li><a data-navigo href="Contact">Contact Us</a></li>
+      <li><a data-navigo href="Help">Help</a></li>
     </ul>
 
   </nav>`;
