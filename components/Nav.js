7161c06c561598aa28b8955dd89cb1012fdd89ae
Ezra Rabinsky <ezrarabinsky@Ezras-MacBook-Pro.local>
2019-10-22 11:06:15 -0500

    Miscellaneous debugging in Nav.js

--- components/Nav.js
+++ components/Nav.js
@@ -1,10 +1,12 @@
-export default () =>
-  `<nav>
-    <div class="fas fa-bars"
+export default () =>`
+<nav>
+    <span class="fas fa-bars">
+    </span>
     <ul class= "navLinks">
-      <li><a href="Profile"></a>My Profile</li>
+      <li><a href="Profile">My Profile</a></li>
       <li><a href="Reservations">My Reservations</a></li>
       <li><a href="Contact">Contact Us</a></li>
       <li><a href="Help">Help</a></li>
+    </ul>
 
   </nav>`;
