ba5f3c23c7595c0f83823b38807c72ce58bad4a5
Ezra Rabinsky <ezra.rabinsky@gmail.com>
2019-10-26 21:41:48 -0500

    Refactor render function to include navSlide

--- index.js
+++ index.js
@@ -6,28 +6,6 @@ import Navigo from "navigo";
 const router = new Navigo(location.origin)
 
 
-
-//renders imported content as state within specified components
-function render(st = state.Login){
-
-  (document.querySelector("#root").innerHTML = `
-  ${Header(st)}
-  ${Nav(st.navLinks)}
-  ${Main(st)}
-  ${Footer(st)}
-  `);
-router.updatePageLinks();
-};
-
-
-router
-.on(":page", params =>
-render(state[`${params.page.slice(0, 1).toUpperCase()}${params.page.slice(1).toLowerCase()}`])
-)
-.on("/", () => render())
-.resolve();
-
-
 // TODO: Add querySelectors and addEventListener to make hamburger icon functional
 
 const navSlide = () => {
@@ -40,11 +18,29 @@ const navSlide = () => {
     nav.classList.toggle('nav-links-active');
   })
 }
+
+
+//renders imported content as state within specified components
+function render(st = state.Login){
+
+  (document.querySelector("#root").innerHTML = `
+  ${Header(st)}
+  ${Nav(st.navLinks)}
+  ${Main(st)}
+  ${Footer(st)}
+  `);
+router.updatePageLinks();
 navSlide();
 
+};
 
 
-  render(st);
+router
+.on(":page", params =>
+render(state[`${params.page.slice(0, 1).toUpperCase()}${params.page.slice(1).toLowerCase()}`])
+)
+.on("/", () => render())
+.resolve();
 
 
 
